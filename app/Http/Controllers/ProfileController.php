aa2e2c69b21ad78f6b936573c9e68781aff7e7b1
Mizuki <mizuki793@users.noreply.github.com>
2020-12-08 15:56:11 +0900

    profileページを追加

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProfileController extends Controller
+{
+    public function show(){
+
+        if(!Auth::check()){
+            return redirect()->route('login');
+        }
+
+        $user =  \Auth::user();
+        return view('profile.index',compact('user'));     
+    }
+}
