1fa22508cb6ada29a99b96ab182f1c9efaded89e
Ryo-M-Code <ryo.m.code@gmail.com>
2021-01-10 23:16:43 +0900

    テストでのログイン処理を静的関数として切り出す

--- tests/Feature/Auth0Test.php
+++ tests/Feature/Auth0Test.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 namespace Tests\Feature;
 
-use Auth0\Login\Auth0JWTUser;
-use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
+use Tests\Utils\AuthUtil;
 
 class Auth0Test extends TestCase
 {
@@ -40,7 +39,7 @@ class Auth0Test extends TestCase
      */
     public function an_auth_user_can_see_the_logout_button(): void
     {
-        $user = $this->createDummyAuthUser();
+        $user = AuthUtil::createDummyAuthUser();
         $this->be($user);
 
         $response = $this->get('/');
@@ -53,7 +52,7 @@ class Auth0Test extends TestCase
      */
     public function an_auth_user_cannot_see_the_login_button(): void
     {
-        $user = $this->createDummyAuthUser();
+        $user = AuthUtil::createDummyAuthUser();
         $this->be($user);
 
         $response = $this->get('/');
@@ -61,24 +60,4 @@ class Auth0Test extends TestCase
             ->assertDontSee('ログイン');
     }
 
-    /**
-     * ダミーのログインユーザーを作成して返却.
-     * @return null|\Illuminate\Contracts\Auth\Authenticatable
-     */
-    private function createDummyAuthUser()
-    {
-        $dummyAuth0User = [
-            'sub' => 'auth0|xxxx|xxxxxxxx',
-            'nickname' => 'nickname',
-            'name' => 'user name',
-            'picture' => 'https://example.com/picture.png',
-            'updated_at' => '2020-11-29T08:10:37.855Z',
-            'email' => 'test@example.com',
-        ];
-
-        $auth0 = new Auth0JWTUser($dummyAuth0User);
-        Auth::setUser($auth0);
-
-        return Auth::user();
-    }
 }
---
+++ tests/Utils/AuthUtil.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Utils;
+
+use Auth0\Login\Auth0JWTUser;
+use Illuminate\Support\Facades\Auth;
+
+class AuthUtil
+{
+    /**
+     * ダミーのログインユーザーを作成して返却.
+     * @return null|\Illuminate\Contracts\Auth\Authenticatable
+     */
+    public static function createDummyAuthUser()
+    {
+        $dummyAuth0User = [
+            'sub' => 'auth0|xxxx|xxxxxxxx',
+            'nickname' => 'nickname',
+            'name' => 'user name',
+            'picture' => 'https://example.com/picture.png',
+            'updated_at' => '2020-11-29T08:10:37.855Z',
+            'email' => 'test@example.com',
+        ];
+
+        $auth0 = new Auth0JWTUser($dummyAuth0User);
+        Auth::setUser($auth0);
+
+        return Auth::user();
+    }
+}
