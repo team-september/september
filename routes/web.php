7d0c0064d607a3a0677091a1b3d2234170a88e0d
Ryo-M-Code <ryo.m.code@gmail.com>
2021-01-22 21:39:40 +0900

    Routeに名前を付与

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,12 @@ Route::get('/login', 'Auth\Auth0IndexController@login')->name('login');
 Route::get('/logout', 'Auth\Auth0IndexController@logout')->name('logout')->middleware('auth');
 
 // ログインしないと使えない機能はすべて以下に記載するように
-Route::group(['middleware' => ['auth']], function() {
-    Route::get('/profile', 'ProfileController@show')->name('profile');
-    Route::get('/profile/edit/{id}','ProfileController@edit');
-    Route::put('/profile/update/{id}','ProfileController@update');    
-});
+Route::group(
+    ['middleware' => ['auth']],
+    function () {
+        Route::get('/profile', 'ProfileController@show')->name('profile.show');
+        Route::get('/profile/edit/{id}', 'ProfileController@edit')->name('profile.edit');
+        Route::put('/profile/update/{id}', 'ProfileController@update')->name('profile.update');
+    }
+);
 
