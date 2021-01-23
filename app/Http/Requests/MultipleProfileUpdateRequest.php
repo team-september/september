80f036b00b574693fb8b7499835d8a52eca3aae0
Ryo-M-Code <ryo.m.code@gmail.com>
2021-01-23 17:27:48 +0900

    FormRequestを追加

---
+++ app/Http/Requests/MultipleProfileUpdateRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MultipleProfileUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $formRequests = [
+            UserUpdateRequest::class,
+            ProfileUpdateRequest::class,
+            UrlUpdateRequest::class,
+            SkillUpdateRequest::class,
+            PurposeUpdateRequest::class,
+        ];
+
+        $rules = [];
+
+        foreach ($formRequests as $source) {
+            $rules = array_merge(
+                $rules,
+                (new $source)->rules()
+            );
+        }
+
+        return $rules;
+    }
+}
