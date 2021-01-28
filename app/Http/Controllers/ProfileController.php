19fa403074fc627268c458a048179eb4f3a2fc2d
Mizuki <mizuki793@users.noreply.github.com>
2021-01-28 17:36:31 +0900

    Revert "チェックボックスで選択できるようにeditとcontrollerを変更"
    This reverts commit 2d2a7bbf8f7d4fc54c9f5a659f67949fe25dc788.

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,8 +8,6 @@ use App\Http\Requests\MultipleProfileUpdateRequest;
 use App\Models\User;
 use App\Models\Career;
 use App\Services\UrlService;
-use App\Models\Purpose;
-use App\Models\Skill;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
@@ -40,6 +38,7 @@ class ProfileController extends Controller
         $career = $profile->career;
         $purposes = $profile->purposes;
         $skills = $profile->skills;
+
         return view(
             'profile.index',
             compact('user', 'profile', 'urls', 'career', 'purposes', 'skills')
@@ -53,15 +52,12 @@ class ProfileController extends Controller
         $urls = (new UrlService($profile))->findUrls();
         $user_career = $profile->career;
         $careers = Career::all();
-        $purposes = Purpose::all();
-        $careers = Career::all();
-        $skills = Skill::all();
-        $user_purposes = $profile->purposes;
-        $user_skills = $profile->skills;
+        $purposes = $profile->purposes;
+        $skills = $profile->skills;
 
         return view(
             'profile.edit',
-            compact('user', 'profile', 'urls', 'user_career', 'careers', 'user_purposes', 'purposes', 'user_skills', 'skills')
+            compact('user', 'profile', 'urls', 'user_career', 'careers', 'purposes', 'skills')
         );
     }
 
@@ -83,4 +79,5 @@ class ProfileController extends Controller
 
         return redirect()->route('profile.index')->with('success', '更新しました');
     }
+
 }
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -1,171 +1,181 @@
 @extends('layouts.base')
 
 @section('content')
-<div class="container mt-5">
-    <div class="main-body">
-        @if (session('success'))
-        <div class="alert alert-success text-center">
-            <ul class="list-unstyled">
-                <li>{{ session('success') }}</li>
-            </ul>
-        </div>
-        @endif
-        <div class="row gutters-sm">
-            <div class="col-md-4 mb-3">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="d-flex flex-column align-items-center text-center">
-                            <img src="{{ $user->picture }}" alt="Admin" class="rounded-circle" width="150">
-                            <div class="mt-3">
-                                <h4>{{ $user->name }}</h4>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card mt-3">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                            <h6 class="mb-0">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github mr-2 icon-inline">
-                                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
-                                </svg>
-                                Github
-                            </h6>
-                            <span class="text-secondary">{{ $urls['github']->url ?? '未登録' }}</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                            <h6 class="mb-0">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-twitter mr-2 icon-inline text-info">
-                                    <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
-                                </svg>
-                                Twitter
-                            </h6>
-                            <span class="text-secondary">{{ $urls['twitter']->url ?? '未登録' }}</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                            <h6 class="mb-0">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor mr-2 icon-inline">
-                                    <circle cx="12" cy="12" r="10"></circle>
-                                    <line x1="2" y1="12" x2="22" y2="12"></line>
-                                    <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
-                                </svg>
-                                Website
-                            </h6>
-                            <span class="text-secondary">{{ $urls['website']->url ?? '未登録' }}</span>
-                        </li>
+    <div class="container mt-5">
+        <div class="main-body">
+            @if (session('success'))
+                <div class="alert alert-success text-center">
+                    <ul class="list-unstyled">
+                        <li>{{ session('success') }}</li>
                     </ul>
                 </div>
-            </div>
-            <div class="col-md-8">
-                <div class="card mb-3">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">名前</h6>
-                            </div>
-                            <div class="col-sm-9 text-secondary">
-                                {{ $user->name }}
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">エンジニア歴</h6>
-                            </div>
-                            <div class="col-sm-9 text-secondary">
-                                {{ $career->year ?? '未登録' }}
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">利用目的</h6>
-                            </div>
-                            <div class="col-sm-9 text-secondary">
-                                {{ $profile->goal ?? '未登録' }}
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">希望</h6>
-                            </div>
-                            <div class="col-sm-9 text-secondary">
-                                @if($purposes->isEmpty())
-                                未登録
-                                @else
-                                @foreach($purposes as $purpose)
-                                <div class="col-sm-5 text-secondary">
-                                    <li> {{ $purpose->purpose_name }} </li>
+            @endif
+            <div class="row gutters-sm">
+                <div class="col-md-4 mb-3">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex flex-column align-items-center text-center">
+                                <img src="{{ $user->picture }}" alt="Admin" class="rounded-circle" width="150">
+                                <div class="mt-3">
+                                    <h4>{{ $user->name }}</h4>
                                 </div>
-                                @endforeach
-                                @endif
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">スキル</h6>
-                            </div>
-                            <div class="col-sm-9 text-secondary flex">
-                                @if($skills->isEmpty())
-                                未登録
-                                @else
-                                @foreach($skills as $skill)
-                                @if ($skill['skill_type'] === 1)
-                                <span class="badge badge-warning">{{ $skill['skill_name'] }}</span>
-                                @elseif($skill['skill_type'] === 2)
-                                <span class="badge badge-success">{{ $skill['skill_name'] }}</span>
-                                @elseif($skill['skill_type'] === 3)
-                                <span class="badge badge-info">{{ $skill['skill_name'] }}</span>
-                                @endif
-                                @endforeach
-                                @endif
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">その他URL</h6>
-                            </div>
-                            <div class="col-sm-9 text-secondary">
-                                {{ $urls['other']->url ?? '未登録' }}
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">ユーザー種別</h6>
-                            </div>
-                            <div class="col-sm-9 text-secondary">
-                                メンティー
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">ステータス</h6>
-                            </div>
-                            <div class="col-sm-9 text-secondary">
-                                進行中（2021/09/01~）
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-sm-3">
-                                <h6 class="mb-0">メンター</h6>
-                            </div>
-                            <div class="col-sm-9 text-secondary">
-                                Yuta Nakano
                             </div>
                         </div>
                     </div>
+                    <div class="card mt-3">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                <h6 class="mb-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                         stroke-linejoin="round" class="feather feather-github mr-2 icon-inline">
+                                        <path
+                                            d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                                    </svg>
+                                    Github
+                                </h6>
+                                <span class="text-secondary">{{ $urls['github']->url ?? '未登録' }}</span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                <h6 class="mb-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                         stroke-linejoin="round"
+                                         class="feather feather-twitter mr-2 icon-inline text-info">
+                                        <path
+                                            d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+                                    </svg>
+                                    Twitter
+                                </h6>
+                                <span class="text-secondary">{{ $urls['twitter']->url ?? '未登録' }}</span>
+                            </li>
+                            <li class="list-group-item d-flex justify-content-between align-items-center flex-wrap">
+                                <h6 class="mb-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                         fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                         stroke-linejoin="round" class="feather feather-monitor mr-2 icon-inline">
+                                        <circle cx="12" cy="12" r="10"></circle>
+                                        <line x1="2" y1="12" x2="22" y2="12"></line>
+                                        <path
+                                            d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
+                                    </svg>
+                                    Website
+                                </h6>
+                                <span class="text-secondary">{{ $urls['website']->url ?? '未登録' }}</span>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="row justify-content-center px-3">
-                    <a href="{{ route('profile.edit', $user->id) }}" class="btn btn-primary col-md-4"> 編集 </a>
+                <div class="col-md-8">
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">名前</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    {{ $user->name }}
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">エンジニア歴</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    {{ $career->year ?? '未登録' }}
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">利用目的</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    {{ $profile->goal ?? '未登録' }}
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">希望</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    @if($purposes->isEmpty())
+                                        未登録
+                                    @else
+                                        @foreach($purposes as $purpose)
+                                            <div class="col-sm-5 text-secondary">
+                                                <li> {{ $purpose }} </li>
+                                            </div>
+                                        @endforeach
+                                    @endif
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">スキル</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary flex">
+                                    @if($skills->isEmpty())
+                                        未登録
+                                    @else
+                                        @foreach($skills as $skill)
+                                            @if ($skill['skill_type']  === 1)
+                                                <span class="badge badge-warning">{{ $skill['skill_name'] }}</span>
+                                            @elseif($skill['skill_type']  === 2)
+                                                <span class="badge badge-success">{{ $skill['skill_name'] }}</span>
+                                            @elseif($skill['skill_type']  === 3)
+                                                <span class="badge badge-info">{{ $skill['skill_name'] }}</span>
+                                            @endif
+                                        @endforeach
+                                    @endif
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">その他URL</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    {{ $urls['other']->url ?? '未登録' }}
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">ユーザー種別</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    メンティー
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">ステータス</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    進行中（2021/09/01~）
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-sm-3">
+                                    <h6 class="mb-0">メンター</h6>
+                                </div>
+                                <div class="col-sm-9 text-secondary">
+                                    Yuta Nakano
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-center px-3">
+                        <a href="{{ route('profile.edit', $user->id) }}" class="btn btn-primary col-md-4"> 編集 </a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
