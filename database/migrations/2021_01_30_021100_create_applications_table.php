f8b417c69efb809b7920c95234e225a231fc1918
Ryo-M-Code <ryo.m.code@gmail.com>
2021-01-30 16:13:32 +0900

    applicationsテーブルを修正

--- database/migrations/2021_01_30_021100_create_applications_table.php
+++ database/migrations/2021_01_30_021100_create_applications_table.php
@@ -17,8 +17,10 @@ class CreateApplicationsTable extends Migration
             'applications',
             function (Blueprint $table) {
                 $table->id();
-                $table->unsignedBigInteger('mentee_id');
-                $table->unsignedBigInteger('mentor_id');
+                $table->unsignedBigInteger('mentee_id')->comment('メンティーID');
+                $table->unsignedBigInteger('mentor_id')->comment('メンターID');
+                $table->unsignedTinyInteger('status')->comment('応募ステータス');
+                $table->timestamp('approved_at')->nullable()->comment('承認日');
                 $table->timestamps();
 
                 $table->foreign('mentee_id')
