12037150a0dd69ad63145b9eb1b33d1627a34ba7
Mizuki <mizuki793@users.noreply.github.com>
2021-02-08 17:36:03 +0900

    no02暗黙の結合追加

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -16,6 +16,7 @@ use App\Services\UrlService;
 use App\Services\ProfileService;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class ProfileController extends Controller
 {
@@ -101,9 +102,8 @@ class ProfileController extends Controller
         );
     }
 
-    public function show($id)
+    public function show(User $user)
     {
-        $user = $this->userRepository->getUserById($id);
         $profile = $user->profile;
         $urls = $this->urlService->findUrls($profile, config('url.types'));
         list($career, $purposes, $skills, $mentors, $application, $mentor_applied) = $this->profileService->findProfile($profile);
