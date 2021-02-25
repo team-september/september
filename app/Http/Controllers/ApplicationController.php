17d87fa11cdc435eaf04f54b1159af1d19642282
GitHub Action <action@github.com>
2021-02-25 12:33:51 +0000

    cs-fixer

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -79,13 +79,15 @@ class ApplicationController extends Controller
     public function update(ApplicationUpdateRequest $request)
     {
         $mentorId = $request->mentor_id;
+
         if ($request->has('rejected')) {
             //application statusを3に更新
-            $menteeId= $request->rejected;
+            $menteeId = $request->rejected;
             $this->applicationRepository->updateApprovedApplication($mentorId, $menteeId);
             return redirect()->route('application.index')->with(['alert' => '応募を拒否しました。']);
-        } elseif ($request->has('approved')) {            
+        } elseif ($request->has('approved')) {
             $mentees = $request->userId;
+
             foreach ($mentees as $menteeId) {
                 //aplication statusを2に更新
                 $this->applicationRepository->updateApprovedApplication($mentorId, $menteeId);
