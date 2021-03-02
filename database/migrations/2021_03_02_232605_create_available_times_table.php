d8d88bee706b6d46d4e0640447a2686d449fb7d0
GitHub Action <action@github.com>
2021-03-02 23:49:20 +0000

    cs-fixer

--- database/migrations/2021_03_02_232605_create_available_times_table.php
+++ database/migrations/2021_03_02_232605_create_available_times_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,12 +10,10 @@ class CreateAvailableTimesTable extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('available_times', function (Blueprint $table) {
+        Schema::create('available_times', function (Blueprint $table): void {
             $table->id();
             $table->unsignedBigInteger('availability_id')->comment('予約可能日ID');
             $table->time('time')->comment('予約可能時間');
@@ -28,10 +28,8 @@ class CreateAvailableTimesTable extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('available_times');
     }
