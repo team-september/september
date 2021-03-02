212a7df2cddef060141eff5b823407933bd1f136
OGI <t.ogi.1211@gmail.com>
2021-03-03 01:45:26 +0900

    メンターの空き日設定用のテーブル定義とモデル作成

---
+++ database/migrations/2021_03_02_154959_create_availabilities_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAvailabilitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('availabilities', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('mentor_id')->comment('メンターID');
+            $table->date('available_date')->comment('予約可能日');
+            $table->text('available_times')->comment('予約可能な時間（スラッシュ区切り）');
+            $table->timestamps();
+            $table->foreign('mentor_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('availabilities');
+    }
+}
