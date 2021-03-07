c2689b18d0735db67469ff53676397a1a78e5304
OGI <t.ogi.1211@gmail.com>
2021-03-07 13:26:59 +0900

    cs-fixer

--- database/migrations/2021_03_06_115050_create_mentorships_table.php
+++ database/migrations/2021_03_06_115050_create_mentorships_table.php
@@ -1,22 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class CreateMentorshipsTable extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
         Schema::dropIfExists('mentorships'); // mentorships実テーブルが環境にあれば一旦ドロップ
 
         DB::statement('DROP VIEW IF EXISTS mentorships');
-        DB::statement("CREATE VIEW
+        DB::statement('CREATE VIEW
                             mentorships
                         AS 
                             SELECT mentor_id, mentee_id, (CASE 
@@ -24,15 +24,13 @@ class CreateMentorshipsTable extends Migration
                                                             ELSE false
                                                           END) as is_active 
                             FROM applications  
-          ");
+          ');
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
         DB::statement('DROP VIEW IF EXISTS mentorships');
     }
