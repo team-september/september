e7b99e6dd4f017b0c71f39adce8402cbad4106cb
OGI <t.ogi.1211@gmail.com>
2021-03-07 16:14:09 +0900

    ApplicationsTableSeederをDatabaseSeederに登録

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,5 +18,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PurposeTableSeeder::class);
         $this->call(SkillTableSeeder::class);
         $this->call(UserTableSeeder::class);
+        $this->call(ApplicationsTableSeeder::class);
     }
 }
