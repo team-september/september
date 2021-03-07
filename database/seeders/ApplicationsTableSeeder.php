bf6226c437aeddf0c6de268648b9ce3d8e7d392f
OGI <t.ogi.1211@gmail.com>
2021-03-07 16:12:04 +0900

    Applications用ダミーデータの作成　Seeder

---
+++ database/seeders/ApplicationsTableSeeder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Seeders;
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ApplicationsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $dummyData = [
+            [
+                'mentee_id' => 2,
+                'mentor_id' => 1,
+                'status' => 2,
+                'approved_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'mentee_id' => 3,
+                'mentor_id' => 1,
+                'status' => 3,
+                'approved_at' => null,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'mentee_id' => 4,
+                'mentor_id' => 1,
+                'status' => 4,
+                'approved_at' => Carbon::now(),
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'mentee_id' => 5,
+                'mentor_id' => 1,
+                'status' => 1,
+                'approved_at' => null,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+        ];
+
+        DB::table('applications')->insert($dummyData);
+    }
+}
