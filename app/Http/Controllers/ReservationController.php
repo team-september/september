e8948e32030b80ff7fdb03b09e5267a28943995e
OGI <t.ogi.1211@gmail.com>
2021-03-17 00:45:51 +0900

    SEPT-62 CalendarServiceクラスリファクタ
    - 役割を再考　⇒　クラスリネーム
    - オブジェクトでまとめて返すように変更、ゲッターメソッド削除
    - Controllerの記述変更

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -4,19 +4,21 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Services\CalendarService;
+use App\Services\AvailabilityService;
 use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
     public function index(Request $request)
     {
-        $Service = new CalendarService($request);
-        $prevMonth = $Service->getPrevMonth()->format('Y-m');
-        $nextMonth = $Service->getNextMonth()->format('Y-m');
-        $currentMonth = $Service->getCurrentMonth()->format('Y年m月');
-        $calendarData = $Service->getCalendarData();
+        $Service = new AvailabilityService($request);
+        $availabilityData = $Service->getAvailabilityDataByMonth();
 
-        return view('reservation.index', compact('prevMonth', 'nextMonth', 'currentMonth', 'calendarData'));
+        return view('reservation.index', [
+            'prevMonth'    => $availabilityData->prevMonth->format('Y-m'),
+            'nextMonth'    => $availabilityData->nextMonth->format('Y-m'),
+            'currentMonth' => $availabilityData->currentMonth->format('Y年m月'),
+            'calendarData' => $availabilityData->weeks,
+        ]);
     }
 }
--- app/Services/AvailabilityService.php
+++ app/Services/AvailabilityService.php
@@ -7,16 +7,16 @@ namespace App\Services;
 use App\Models\User;
 use App\Repositories\Availability\IAvailabilityRepository;
 use App\Repositories\Mentorship\IMentorshipRepository;
+use App\Repositories\User\IUserRepository;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Auth;
 
 /**
- * 現在ログイン中のユーザーに合わせてカレンダー用のデータを返すクラス
- * 1. メンティーの場合は紐づいたメンターの予定
- * 2. メンターの場合は過去の日付以外は設定のリンク表示.
+ * 現在ログイン中のユーザーに合わせて対象月の1on1予約可能日のデータを返すクラス.
  */
-class CalendarService
+class AvailabilityService
 {
     public User $user;
 
@@ -30,18 +30,17 @@ class CalendarService
 
     protected Carbon $prevMonth;
 
-    public function __construct(User $user, Request $request)
+    public function __construct(Request $request)
     {
-        $this->user = $user;
-
         // 日付系のデータ取得／加工
         $this->Carbon = $request->ym ? new Carbon($request->ym) : Carbon::now();
         $this->firstDayOfMonth = $this->Carbon->copy()->firstOfMonth();
         $this->lastDayOfMonth = $this->Carbon->copy()->lastOfMonth();
         $this->prevMonth = $this->Carbon->copy()->subMonthsNoOverflow();
         $this->nextMonth = $this->Carbon->copy()->addMonthsNoOverflow();
-
+        
         // メンターID取得（メンティーの場合は担当メンター）
+        $this->user = app(IUserRepository::class)->getUserBySub(Auth::id());
         $mentor_id = $this->user->is_mentor ? $this->user->id : app(IMentorshipRepository::class)->getMentorIdByMenteeId($this->user->id);
 
         // メンターに紐づいた予約可能日取得
@@ -49,54 +48,48 @@ class CalendarService
     }
 
     /**
-     * @return Carbon $this->Carbon
+     * 当月のデータ取得
+     * @return object 
+     * @properties
+     * Carbon $object->currentMonth
+     * Carbon $object->nextMonth
+     * Carbon $object->prevMonth
+     * Array  $object->weeks->days
+     * @see $this->getDays()
      */
-    public function getCurrentMonth(): Carbon
-    {
-        return $this->Carbon;
-    }
-
-    /**
-     * @return Carbon $this->prevMonth
-     */
-    public function getPrevMonth(): Carbon
-    {
-        return $this->prevMonth;
-    }
-
-    /**
-     * @return Carbon $this->prevMonth
-     */
-    public function getNextMonth(): Carbon
-    {
-        return $this->nextMonth;
-    }
-
-    /**
-     * 日付データの取得.
-     *
-     * @return array $weeks
-     */
-    public function getCalendarData(): array
+    public function getAvailabilityDataByMonth(): object
     {
         $weeks = [];
         $tmpDay = $this->firstDayOfMonth->copy();
 
         // 月末までループ
         while ($tmpDay->lte($this->lastDayOfMonth)) {
-            $weeks[] = $this->getDays($tmpDay);
+            $weeks[] = $this->getDaysByWeek($tmpDay);
             $tmpDay->addDay(7);
         }
 
-        return $weeks;
+        return (object) [
+            'currentMonth' => $this->Carbon,
+            'nextMonth'    => $this->nextMonth,
+            'prevMonth'    => $this->prevMonth,
+            'weeks'        => $weeks,
+        ];
     }
 
     /**
-     * 日付文字列配列.
+     * 週毎のデータを配列化したもの
      * @param Carbon $date
      * @return array $days
+     * 
+     * day object properties
+     * date         => Y-m-d
+     * date_j       => 1 - 31
+     * day          => mon - sun
+     * is_today     => bool
+     * is_past      => bool
+     * is_available => bool
      */
-    private function getDays(Carbon $date): array
+    private function getDaysByWeek(Carbon $date): array
     {
         $tmpDay = $date->copy()->startOfWeek(); // 週の頭にセット
 
@@ -104,7 +97,7 @@ class CalendarService
         $endOfWeek = $date->copy()->endOfWeek();
         $days = [];
         while ($tmpDay->lte($endOfWeek)) {
-            //前の月、もしくは後ろの月の場合は空文字
+            //前の月、もしくは後ろの月の場合はnull
             if ($tmpDay->month !== $this->Carbon->month) {
                 $days[] = null;
                 $tmpDay->addDay(1);
