557f592ef420f348f3f93ba1837d835236f3ed25
OGI <t.ogi.1211@gmail.com>
2021-03-25 00:01:57 +0900

    SEPT-62 サービスクラスのリポジトリの利用をDIに変更

--- app/Services/AvailabilityService.php
+++ app/Services/AvailabilityService.php
@@ -21,13 +21,18 @@ class AvailabilityService
 
     protected int $mentor_id;
 
-    protected Carbon $date;
+    protected Carbon $carbon;
 
-    public function __construct()
+    public function __construct(
+        IUserRepository $userRepository,
+        IMentorshipRepository $mentorshipRepository,
+        IAvailabilityRepository $availabilityRepository
+        )
     {
+        $this->availabilityRepository = $availabilityRepository;
         // メンターID取得（メンティーの場合は担当メンター）
-        $this->user = app(IUserRepository::class)->getUserBySub(Auth::id());
-        $this->mentor_id = $this->user->is_mentor ? $this->user->id : app(IMentorshipRepository::class)->getMentorIdByMenteeId($this->user->id);
+        $this->user = $userRepository->getUserBySub(Auth::id());
+        $this->mentor_id = $this->user->is_mentor ? $this->user->id : $mentorshipRepository->getMentorIdByMenteeId($this->user->id);
     }
 
     /**
@@ -43,13 +48,13 @@ class AvailabilityService
      */
     public function getAvailabilityDataByMonth(Carbon $date): object
     {
-        $this->Carbon = $date;
+        $this->carbon = $date;
         // メンターに紐づいた当月の予約可能日取得
-        $this->Availabilities = app(IAvailabilityRepository::class)->getMonthsAvailabilitiesByDate($date, $this->mentor_id);
-        $firstDayOfMonth = $date->copy()->firstOfMonth();
-        $lastDayOfMonth = $date->copy()->lastOfMonth();
-        $prevMonth = $date->copy()->subMonthsNoOverflow();
-        $nextMonth = $date->copy()->addMonthsNoOverflow();
+        $this->availabilities = $this->availabilityRepository->getMonthsAvailabilitiesByDate($this->carbon, $this->mentor_id);
+        $firstDayOfMonth = $this->carbon->copy()->firstOfMonth();
+        $lastDayOfMonth = $this->carbon->copy()->lastOfMonth();
+        $prevMonth = $this->carbon->copy()->subMonthsNoOverflow();
+        $nextMonth = $this->carbon->copy()->addMonthsNoOverflow();
 
         $weeks = [];
         $tmpDay = $firstDayOfMonth;
@@ -61,7 +66,7 @@ class AvailabilityService
         }
 
         return (object) [
-            'currentMonth' => $date,
+            'currentMonth' => $this->carbon,
             'nextMonth' => $nextMonth,
             'prevMonth' => $prevMonth,
             'weeks' => $weeks,
@@ -77,14 +82,14 @@ class AvailabilityService
         $dates = collect($request->availability_setting);
 
         $deleteTargets = $dates->where('is_available', false);
-        $deleteResult = app(IAvailabilityRepository::class)->removeAvailabilitiesByDates($deleteTargets, $this->mentor_id);
+        $deleteResult = $this->availabilityRepository->removeAvailabilitiesByDates($deleteTargets, $this->mentor_id);
 
         if ($deleteResult === false) {
             return back()->withErrors('更新処理に失敗しました');
         }
 
         $updateTargets = $dates->where('is_available', true);
-        return app(IAvailabilityRepository::class)->updateOrInsertAvailabilitiesByDates($updateTargets, $this->mentor_id);
+        return $this->availabilityRepository->updateOrInsertAvailabilitiesByDates($updateTargets, $this->mentor_id);
     }
 
     public function updateAvailableTimes(Request $request)
@@ -96,7 +101,7 @@ class AvailabilityService
 
         $times = collect($request->set_time);
 
-        return app(IAvailabilityRepository::class)->updateOrInsertAvailableTimes($times, $this->mentor_id);
+        return $this->availabilityRepository->updateOrInsertAvailableTimes($times, $this->mentor_id);
     }
 
     /**
@@ -121,7 +126,7 @@ class AvailabilityService
         $days = [];
         while ($tmpDay->lte($endOfWeek)) {
             //前の月、もしくは後ろの月の場合はnull
-            if ($tmpDay->month !== $this->Carbon->month) {
+            if ($tmpDay->month !== $this->carbon->month) {
                 $days[] = null;
                 $tmpDay->addDay(1);
                 continue;
@@ -136,10 +141,10 @@ class AvailabilityService
             ];
 
             // is_availableフラグ
-            if ($this->Availabilities->isEmpty()) {
+            if ($this->availabilities->isEmpty()) {
                 $day['is_available'] = false;
             } else {
-                $day['is_available'] = $this->Availabilities->filter(
+                $day['is_available'] = $this->availabilities->filter(
                     function ($date) use ($day) {
                         return $date->available_date->format('Y-m-d') == $day['date'];
                     }
