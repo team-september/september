37a7b6d192ad6f96f981237b9ddd6cb5d1a08906
GitHub Action <action@github.com>
2021-04-01 14:46:22 +0000

    cs-fixer

--- database/migrations/2021_04_01_223256_alter_available_times.php
+++ database/migrations/2021_04_01_223256_alter_available_times.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -8,15 +10,13 @@ class AlterAvailableTimes extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::table('available_times', function (Blueprint $table) {
+        Schema::table('available_times', function (Blueprint $table): void {
             $table->dropForeign('available_times_availability_id_foreign');
             $table->foreign('availability_id')
-            ->references('id')
+                ->references('id')
                 ->on('availabilities')
                 ->onDelete('cascade');
         });
@@ -24,15 +24,13 @@ class AlterAvailableTimes extends Migration
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::table('available_times', function (Blueprint $table) {
+        Schema::table('available_times', function (Blueprint $table): void {
             $table->dropForeign('available_times_availability_id_foreign');
             $table->foreign('availability_id')
-            ->references('id')
+                ->references('id')
                 ->on('user')
                 ->onDelete('cascade');
         });
