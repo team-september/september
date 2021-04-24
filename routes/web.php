d49bd60e0d6c9f826c838aacc340f69adbf89adf
Ryo-M-Code <ryo.m.code@gmail.com>
2021-04-24 18:12:36 +0900

    他のroutingもグループ化

--- routes/web.php
+++ routes/web.php
@@ -24,18 +24,21 @@ Route::get('/logout', 'Auth\Auth0IndexController@logout')->name('logout')->middl
 Route::group(
     ['middleware' => ['auth']],
     function (): void {
-        Route::get('/profile', 'ProfileController@index')->name('profile.index');
-        Route::get('/profile/show/{id}', 'ProfileController@show')->name('profile.show');
-        Route::get('/profile/edit', 'ProfileController@edit')->name('profile.edit');
-        Route::put('/profile/update/{id}', 'ProfileController@update')->name('profile.update');
+        Route::group(['prefix' => 'profile'], function (): void {
+            Route::get('/', 'ProfileController@index')->name('profile.index');
+            Route::get('/show/{id}', 'ProfileController@show')->name('profile.show');
+            Route::get('/edit', 'ProfileController@edit')->name('profile.edit');
+            Route::put('/update/{id}', 'ProfileController@update')->name('profile.update');
+        });
 
-        Route::get('/application', 'ApplicationController@index')->name('application.index');
-        Route::post('/application', 'ApplicationController@store')->name('application.store');
-        Route::post('/application/update', 'ApplicationController@update')->name('application.update');
+        Route::group(['prefix' => 'application'], function (): void {
+            Route::get('/', 'ApplicationController@index')->name('application.index');
+            Route::post('/', 'ApplicationController@store')->name('application.store');
+            Route::post('/update', 'ApplicationController@update')->name('application.update');
+        });
 
         Route::group(['prefix' => 'reservation'], function (): void {
-            Route::get('', 'ReservationController@index')->name('reservation.index');
-            Route::get('/check', 'ReservationController@checkReservationApplication')->name('reservation.check');
+            Route::get('/mentor', 'ReservationController@index')->name('reservation.index');
 
             Route::post('/submit', 'ReservationController@reserve')->name('reservation.submit');
             Route::post('/setting', 'ReservationController@setting')->name('reservation.setting');
@@ -44,7 +47,7 @@ Route::group(
             // ajax用エンドポイント
             Route::post('/getAvailability', 'Api\AvailabilityController@getAvailability');
             Route::get('/detail', 'ReservationController@reserve')->name('reservation.reserve');
-
         });
+        
     }
 );
