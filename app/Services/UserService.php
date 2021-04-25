474c6ef553a4734b80eb0d1fce2bc516e614325d
Ryo-M-Code <ryo.m.code@gmail.com>
2021-04-25 13:11:20 +0900

    UserServiceクラスを追加

---
+++ app/Services/UserService.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Services;
+
+
+use App\Repositories\User\IUserRepository;
+
+class UserService
+{
+    protected $userRepository;
+
+    /**
+     * UserService constructor.
+     *
+     * @param $userRepository
+     */
+    public function __construct(IUserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function isMentor(string $sub)
+    {
+        return $this->userRepository->getUserBySub($sub)->is_mentor ? true : false;
+    }
+}
